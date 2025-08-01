0715fcfccad282662d695b56562c8b919107dbbd
Tu Nombre <usuario@ejemplo.com>
2025-08-01 19:44:33 -0300

    feat(backend): Set JWT expiration to 1 hour

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { JwtStrategy } from './jwt.strategy';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '60s' },
+        signOptions: { expiresIn: '1h' },
       }),
       inject: [ConfigService],
     }),
