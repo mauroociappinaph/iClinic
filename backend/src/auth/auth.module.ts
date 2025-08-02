3075a4b6d2ae6798bd4c48d19b3ea0638c691bd3
Tu Nombre <usuario@ejemplo.com>
2025-08-01 21:36:21 -0300

    feat(backend): Remove JWT token expiration

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -15,7 +15,6 @@ import { JwtStrategy } from './jwt.strategy';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '1h' },
       }),
       inject: [ConfigService],
     }),
